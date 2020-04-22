283695d6ce21028f53b18b957e8015d61ba671f3
Nelson Lopez <nlc@sendvalu.com>
2020-04-22 15:11:48 +0200

    Variante 10 - Algunos ajustes a la variante 9

--- covid19.py
+++ covid19.py
@@ -176,6 +176,34 @@ def load_data_from_pdf(pdf_file_path_or_url, fileDate):
             csv_df[csv_column_fallecidos] = tt[tt.columns[0]].apply(lambda s: s.split()[0])
             was_found = True
             break
+        if current_case == 0 and len(t.columns) >= 5 and "Confirmados COVID" in t.columns[1] and "Anticuerpos positivos" in t.columns[4]:
+            current_case = 10
+            print('caso 10 - tabla 1')
+            print(t)
+            tt = t.iloc[4:,0:6]
+            tt = tt.dropna(how='all')
+            tt = tt.reset_index(drop=True)
+            tt = tt.fillna(value={tt.columns[4]:0,tt.columns[5]:0})            
+            print(tt)
+            csv_df[csv_column_fecha] = [fileDate] * len(tt)
+            csv_df[csv_column_ccaa] = tt.iloc[:,0:1]
+            tt['c1'] = tt.iloc[:,4:5]
+            tt['c2'] = tt.iloc[:,5:6]
+            tt['c1'] = tt['c1'].astype(str).str.replace(r'\D','')
+            tt['c2'] = tt['c2'].astype(str).str.replace(r'\D','')
+            csv_df[csv_column_casos] = tt['c2'].astype('int32') - tt['c1'].astype('int32')
+            continue
+        if current_case == 10 and len(t.columns) >= 4 and t.columns[3] == 'Fallecidos':
+            print('caso 10 - tabla 2')
+            print(t)
+            tt = t.iloc[2:,3:4]
+            tt = tt.dropna()
+            tt = tt.reset_index(drop=True)
+            print(tt)
+            # esta columna contiene datos de 2 columnas dentro, separados por espacio y los fallecidos son los de la izquierda
+            csv_df[csv_column_fallecidos] = tt[tt.columns[0]].apply(lambda s: s.split()[0])
+            was_found = True
+            break
         if current_case == 0 and len(t.columns) >= 2 and "Confirmados COVID" in t.columns[1] :
             current_case = 8
             print('caso 8 - tabla 1')
@@ -200,6 +228,7 @@ def load_data_from_pdf(pdf_file_path_or_url, fileDate):
             csv_df[csv_column_fallecidos] = tt[tt.columns[0]].apply(lambda s: s.split()[0])
             was_found = True
             break
+
     if not was_found:
         return None
 
